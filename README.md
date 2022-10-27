2ab5b76a654150e892a3704bcf0f78835cd6dcfa
Rod Howard <rod.howard@gmail.com>
2022-10-27 13:18:22 +1100

    tested and working!

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
-# public-ip-fetch
+# node-public-ip
+
+This is a node.js library wrapper to quickly and simply lookup your public IP address.
+
+## Install
+
+```sh
+npm install node-public-ip
+```
+
+## Usage
+
+```js
+import { publicIp } from "node-public-ip";
+
+console.log(await publicIp());
+//=> "12.133.141.187"
+```
+
+## API
+
+### publicIp(options?)
+
+Returns a `Promise<string>` with your public IPv4 or IPv6 address. Rejects on error or timeout. IPv6 addresses may require IPv6 servers `["2620:0:ccc::2", "2620:0:ccd::2"]` (IPv6 feedback help welcome!).
+
+#### options
+
+Type: `object`
+
+##### servers
+
+Type: `array of ipAddress`\
+Default: `[ "208.67.222.222", "208.67.220.220", "208.67.222.220", "208.67.220.222" ]`
+
+This sets the server IP addresses used in the DNS resolver. It is overridden in the Ipv6 method to be `["2620:0:ccc::2", "2620:0:ccd::2"]`.
+
+##### hostname
+
+Type: `string`\
+Default `myip.opendns.com`
+
+This should match with the server ip addresses other possible services could be `"o-o.myaddr.l.google.com"` with servers `["216.239.32.10", "216.239.34.10", "216.239.36.10", "216.239.38.10"]`.
+
+##### timeoutMs
+
+Type: `integer`\
+Default `100` (milliseconds)
+
+This sets the resolver timeout before it tries a different server.
+
+##### triesPerServer
+
+Type: `integer`\
+Default `1`
+
+This sets tries on the resolver, the number of attempts on each server.
--- index.js
+++ index.js
@@ -1,15 +1,34 @@
-const dns = require("node:dns");
-const options = {
-  family: 6,
-  hints: dns.ADDRCONFIG | dns.V4MAPPED,
-};
-dns.lookup("example.com", options, (err, address, family) =>
-  console.log("address: %j family: IPv%s", address, family)
-);
-// address: "2606:2800:220:1:248:1893:25c8:1946" family: IPv6
+import { Resolver } from "node:dns/promises";
 
-// When options.all is true, the result will be an Array.
-options.all = true;
-dns.lookup("example.com", options, (err, addresses) =>
-  console.log("addresses: %j", addresses)
-);
+/**
+ * publicIp uses nodes DNS resolver and services like "myip.opendns.com" to return you're public IP address.
+ * @constructor
+ * @param {object} options - Configuration for the DNS resolver.
+ * @param {array} options.servers - Array of server IP addresses.
+ * @param {string} options.hostname - The hostname of the dns service. Defaults to "myip.opendns.com"
+ * @param {integer} options.timeoutMs - Resolver timeout in milliseconds.
+ * @param {integer} options.triesPerServer - Resolver tries before trying the next server IP address.
+ * @param {function} options.resultTransform - Takes the DNS result and tries to convert it to a single IP address string.
+ */
+const publicIp = ({
+  servers = [
+    "208.67.222.222",
+    "208.67.220.220",
+    "208.67.222.220",
+    "208.67.220.222",
+  ],
+  hostname = "myip.opendns.com",
+  timeoutMs = 100,
+  triesPerServer = 1,
+  resultTransform = (addresses) =>
+    addresses[0]?.address || addresses[0]?.entries?.[0],
+} = {}) => {
+  const resolver = new Resolver({ tries: triesPerServer, timeout: timeoutMs });
+  resolver.setServers(servers);
+  // Resolver results:
+  // o-o.myaddr.l.google.com: [ { entries: [ '121.200.6.176' ], type: 'TXT' } ]
+  // myip.opendns.com: [ { address: '147.182.172.106', ttl: 0, type: 'A' } ]
+  return resolver.resolveAny(hostname).then(resultTransform);
+};
+
+export { publicIp };
